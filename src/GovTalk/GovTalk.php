7cfa67fc167e89114810e011d336dbf335f29a86
Justin Busschau <jbusschau@churchworks.com>
2014-04-12 01:17:42 +0100

    Clean up license boilerplate

--- src/GovTalk/GovTalk.php
+++ src/GovTalk/GovTalk.php
@@ -1,24 +1,4 @@
 <?php
-
-#
-#  GovTalk.php
-#
-#  Created by Jonathon Wardman on 14-07-2009.
-#  Copyright 2009 - 2011, Fubra Limited. All rights reserved.
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  You may obtain a copy of the License at:
-#  http://www.gnu.org/licenses/gpl-3.0.txt
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-
 /**
  * GovTalk API Client -- Builds, validates, sends, receives and validates
  * GovTalk messages for use with the UK government's GovTalk messaging system
